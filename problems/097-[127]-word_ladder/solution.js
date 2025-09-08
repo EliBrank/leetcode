e368f4e90ba574d85a0d3f37e310271b1d116686
EliBrank <eliasbrinkman@gmail.com>
2025-09-07 21:01:40 -0500

    add progress on word ladder solution

---
+++ problems/097-[127]-word_ladder/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+};
+
+
+const beginWord = "tart";
+const endWord = "fine";
+
+const wordList = ["fart", "farm", "firm", "fire", "fine", "star", "mart", "mare", "hare", "harm"];
+
+console.log(ladderLength(beginWord, endWord, wordList));
