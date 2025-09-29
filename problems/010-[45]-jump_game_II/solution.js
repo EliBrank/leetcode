b82d9d922a6a01f5e85d98b365936fd2f2031622
EliBrank <eliasbrinkman@gmail.com>
2025-09-29 09:25:08 -0500

    work on updated jump game ii

--- problems/010-[45]-jump_game_II/solution.js
+++ problems/010-[45]-jump_game_II/solution.js
@@ -3,26 +3,37 @@
  * @return {number}
  */
 export const jump = function(nums) {
-  const reachableIndices = nums.map((num, index) => {
-    return num + index;
-  });
+  if (nums.length === 1) return 0;
 
-  // [ 7,  1, 11, 9, 13, 11, 7, 14, 17, 9, 11, 13, 21, 13, 17 ]
+  const maxIndices = nums.map((num, index) => num + index);
 
-  let current = 0;
-  let window = 0;
-  let hops = 0;
+  let hops = 1;
+  let end = 0;
+  let farthest = 0;
 
-  for (let i = 0; i < nums.length - 1; i++) {
-    current = Math.max(current, reachableIndices[i]);
-    if (i === window) {
-      hops++;
-      window = current;
-    }
+  for (let i = 0; i < nums.length; i++) {
+    farthest = Math.max(fa)
+    reachableIndices = maxIndices.slice(maxIndexReached, maxIndices[maxIndexReached] + 1);
+    maxIndexReached = Math.max(...reachableIndices);
   }
 
   return hops;
 };
 
-const testNums1 = [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3];
+const testNums1 = [2,3,1,1,4];
 console.log(jump(testNums1));
+
+const testNums2 = [2,3,0,1,4];
+console.log(jump(testNums2));
+
+const testNums3 = [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3];
+console.log(jump(testNums3));
+
+const testNums4 = [1,2,3];
+console.log(jump(testNums4));
+
+const testNums5 = [1,2];
+console.log(jump(testNums5));
+
+const testNums6 = [1,1,1,1,1];
+console.log(jump(testNums6));
